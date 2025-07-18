eb4c28ba04a6785eea82f9bae66cb55d395efa99
erik <eriktrujillo0001@gmail.com>
2025-07-18 17:53:47 -0600

    Trabajo

---
+++ src/ejercicio.js
@@ -0,0 +1,54 @@
+// Funciones a evaluar
+
+function suma(a,b) {
+    return a+b;
+}
+
+function esPar(numero){
+    return numero%2 === 0;
+}
+
+function dividir(a,b){
+    if(b === 0) return 'Error';
+    return a/b;
+}
+
+// function obtenerIniciales(nombreCompleto){
+//     const partes = nombreCompleto.split("");
+//     return partes[0][0] + partes[1][0];
+// }
+    
+// function formatearFecha(fechaStr){
+//     // Se espera un formato 'YYYY-MM-DD'
+
+//     const validar = /^\d{4}-\d{2}-\d{2}$/;
+
+//    if (validar.test(fechaStr)) {
+//     const partes = fechaStr.split("-");
+//     return `${partes[2]}/${partes[1]}/${partes[0]}`;
+//    } else {
+//     return "Formato incorrecto";
+//    }
+
+    
+// }
+
+
+//Pruebas unitarias
+
+function test(desc, fn){
+    try {
+        fn();
+        console.log('Correcto '+ desc);
+    } catch (error) {
+        console.error('Fallo ' + desc);
+        console.error(" " + error.message);
+    }
+}
+
+function assertEquals(actual, esperado){
+    if(actual !== esperado){
+        throw new Error (`Esperado: ${esperado} pero obtuvo: ${actual}`);
+    }
+}
+
---
+++ test/operaciones.test.js
@@ -0,0 +1,70 @@
+
+//Suma
+
+test("Suma 5.8 + 0.5 = 6.3", () => {
+    assertEquals(suma(5.8,0.5),6.3);
+})
+
+test("Suma -2+(-2)=-4", () => {
+    assertEquals(suma(-2,-2),-4);
+})
+
+test("Suma -5+3=-2", () => {
+    assertEquals(suma(-5,3),-2);
+})
+
+//esPar
+
+test("Par 2", () => {
+    assertEquals(esPar(2),true);
+})
+
+test("impar 3", () => {
+    assertEquals(esPar(3),false);
+})
+
+test("Par -2", () => {
+    assertEquals(esPar(-2),true);
+})
+
+// dividir
+
+test("Dividir -8/2=-4", () => {
+    assertEquals(dividir(-8,2),-4);
+})
+
+test("Dividir 20.5/2=10.25", () => {
+    assertEquals(dividir(20.5,2),10.25);
+})
+
+// obtener iniciales
+
+// test("Erik Trujillo", () => {
+//     assertEquals(obtenerIniciales('Erik Trujillo'),'E'+'T');
+// })
+
+// test("ErikTrujillo", () => {
+//     assertEquals(obtenerIniciales('ErikTrujillo'),'E'+'T');
+// })
+
+// test("Erik Trujillo Juarez", () => {
+//     assertEquals(obtenerIniciales('Erik Trujillo Juarez'),'E'+'T'+'J');
+// })
+
+// // Formatear fecha
+
+// test("15 de Junio 2001", () => {
+//     assertEquals(formatearFecha('15 de Junio 2001'),'Formato incorrecto');
+// })
+
+// test("15/06/1002", () => {
+//     assertEquals(formatearFecha('15/06/1002'),'Formato incorrecto');
+// })
+
+// test("15-06-1002", () => {
+//     assertEquals(formatearFecha('15-06-1002'),'Formato incorrecto');
+// })
+
+// test("2003-05-05", () => {
+//     assertEquals(formatearFecha('2003-05-05'),'05/05/2003');
+// })
